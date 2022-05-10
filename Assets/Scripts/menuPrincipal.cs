5add318eefda8fab3517771b22cacf0ce6fd25d3
William Foucher <foucher.william1@gmail.com>
2022-05-10 16:01:50 -0400

    Animation du début

--- Assets/Scripts/menuPrincipal.cs
+++ Assets/Scripts/menuPrincipal.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menuPrincipal : MonoBehaviour
 {
+    public Canvas canvas;
+    public Animator mainCamAnim;
+
     public void Jouer()//void pour load scene de jeu quand on click sur jouer
     {
-        SceneManager.LoadScene(1);
+        mainCamAnim.SetBool("animDebut", true);
+        Invoke("LancerPartie", 19f);
+        canvas.enabled = false;
+        gameObject.GetComponent<AudioSource>().volume = 0f;
     }
 
     public void quitter()//void pour quitter le jeu entirement 
     {
         Debug.Log("quitter");
         Application.Quit();
+    }
+    void LancerPartie() {
+        SceneManager.LoadScene(1);
     }
 }
