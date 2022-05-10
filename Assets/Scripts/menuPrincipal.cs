bb39e3885e8d1dda56116ed3fcbf2ea1c03ce629
William Foucher <foucher.william1@gmail.com>
2022-05-10 16:06:22 -0400

    commentaires:)

--- Assets/Scripts/menuPrincipal.cs
+++ Assets/Scripts/menuPrincipal.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 
 public class menuPrincipal : MonoBehaviour
 {
-    public Canvas canvas;
+    // script fait pas Christ Et William, lorsque que je joueur appuis sur JOUER, l'animation du début du jeu se lance
+    // et on désactive tout le reste
+    public Canvas canvas; // référence au canva
     public Animator mainCamAnim;
 
     public void Jouer()//void pour load scene de jeu quand on click sur jouer
     {
         mainCamAnim.SetBool("animDebut", true);
-        Invoke("LancerPartie", 19f);
-        canvas.enabled = false;
+        Invoke("LancerPartie", 19f); //on appel la fonction pour charger la scene principale
+        canvas.enabled = false; // désactive le canvas
         gameObject.GetComponent<AudioSource>().volume = 0f;
     }
 
     public void quitter()//void pour quitter le jeu entirement 
     {
         Debug.Log("quitter");
-        Application.Quit();
+        Application.Quit(); // on quitte le jeu
     }
     void LancerPartie() {
         SceneManager.LoadScene(1);
