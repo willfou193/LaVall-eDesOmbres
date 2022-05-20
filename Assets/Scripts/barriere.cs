ce747e4f02458020b843ec1ee126c194f1e61ea8
William Foucher <e1938665@cmaisonneuve.qc.ca>
2022-05-20 15:10:08 -0400

    script des lumières

--- Assets/Scripts/barriere.cs
+++ Assets/Scripts/barriere.cs
@@ -7,6 +7,7 @@ public class barriere : MonoBehaviour
     public AudioClip generatricePart;
     public AudioClip generatriceEnMarche;
     public GameObject porte;
+    public GameObject[] lumieres;
     public void OuvrirPorte()
     {
         porte.transform.Rotate(0f, 90f, 0f);
@@ -19,5 +20,8 @@ public class barriere : MonoBehaviour
     public void LancerSonIdle(){
         gameObject.GetComponent<AudioSource>().clip = generatriceEnMarche;
         gameObject.GetComponent<AudioSource>().Play();
+        for(int i =0; i < 10; i++){
+                 lumieres[i].SetActive(true);
+             }
     }
 }
